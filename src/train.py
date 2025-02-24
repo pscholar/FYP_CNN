24900e21f60f2b43035bdcc24183364bd9c39b1a
patrick <160159746+pscholar@users.noreply.github.com>
2025-02-24 23:05:37 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -258,19 +258,22 @@ def train_model(dataset_dir):
     last_model_path = model.find_last()
     print("Last trained model path:", last_model_path)
     # Load weights if they exist, otherwise start from scratch
-    if last_model_path:
-        model.load_weights(last_model_path, by_name=True)
-        print("Loaded last trained weights.")
+   last_model_path = ""
+    logs_folder = os.path.join(MODEL_DIR, 'flood_vehicle20250224T2000')
+    if not os.path.exists(logs_folder):
+      print("flood_vehicle20250224T2000 folder does not exist in MODEL_DIR, loading trained heads")
+      try:
+          model.load_weights("/content/drive/My Drive/mask_rcnn_logs/mask_rcnn_flood_vehicle_0023.h5", 
+                            by_name=True)
+      except Exception as e:
+        print("Trained heads don't exist yet, loading coco")
+        model.load_weights("mask_rcnn_coco.h5", 
+                        by_name=True, 
+                        exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                              "mrcnn_bbox", "mrcnn_mask"])
     else:
-        print("No previous weights found. Starting from previously trained heads")
-        try:
-            model.load_weights("/content/drive/My Drive/mask_rcnn_logs/mask_rcnn_flood_vehicle_0023.h5", 
-                             by_name=True)
-        except Exception as e:
-             model.load_weights("mask_rcnn_coco.h5", 
-                             by_name=True, 
-                             exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
-                                    "mrcnn_bbox", "mrcnn_mask"])
+      last_model_path = model.find_last()
+      model.load_weights(last_model_path,by_name=True)
     #Training: head only
     #print("Training network heads...")
     # model.train(dataset_train, dataset_val,
